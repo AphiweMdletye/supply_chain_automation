b3c0576640096083c84bc54457703971d74656ca
Chris Mkhontwana <Chris.mkhontwana@takealot.com>
2019-02-21 13:50:18 +0200

    Updated the EnginDriver
    - Added getSpecificTestData

--- src/main/Engine/EngineDriver.java
+++ src/main/Engine/EngineDriver.java
@@ -1,7 +1,59 @@
-package main.Engine;
+package Engine;
+
+import Utils.ExcelUtility;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class EngineDriver
 {
+    //
+    public static final String XLSX_FILE_PATH = "src/main/DataFiles/userData.xlsx";
+    public  static    String[][] excelData = null;
+    public Map<String, String> currentTestData = null;
+
+    @BeforeClass
+    public void startEngine()throws IOException, InvalidFormatException
+    {
+        excelData = ExcelUtility.ReadExcelFile("Locations");
+    }
 
 
+    @BeforeMethod
+    public void testSetUp(Method method)
+    {
+        String testName =  method.getName();
+        getSpecificTestData(testName);
+    }
+
+
+
+    //Method get a specific test case data from the excelData 2D array
+    public  Map<String, String> getSpecificTestData(String testName) {
+        currentTestData = new LinkedHashMap<String, String>();
+
+        int numRows = excelData.length;
+        int numCols = excelData[0].length;
+
+        System.out.println("Number of rows" + numRows);
+        System.out.println("Number of columns" + numCols);
+
+        for (int i = 0; i < numRows; i++)
+        {
+            if (excelData[i][0].toString().equals(testName))
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    currentTestData.put(excelData[0][j], excelData[i][j]);
+
+                }
+            }
+
+        }
+        return currentTestData;
+    }
 }
