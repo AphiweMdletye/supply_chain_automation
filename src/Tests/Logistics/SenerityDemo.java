a1c781d7ef77ce711ad48f491658050b2b32a483
RynoHoorn <Ryno.Hoorn@Takealot.com>
2019-02-21 10:49:56 +0200

    Add dummy tests for testing the custom headers methods

--- src/Tests/Logistics/SenerityDemo.java
+++ src/Tests/Logistics/SenerityDemo.java
@@ -1,25 +1,65 @@
 package Logistics;
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.testng.Assert;
+import  org.testng.annotations.Test;
 
-public class SenerityDemo
+
+
+
+public class SenerityDemo extends CreateHashMapExample
 {
-    @Test
+
+    static String key,value, singleHeader;
+
+    @Test(enabled = false)
     public void printTestData()
     {
         System.out.print("This is the dummy test case 1");
     }
 
-    @Test
+    @Test(enabled = false)
     public void printTestDataTwo()
     {
         System.out.print("This is the dummy test case 2");
     }
 
-    @Test
+    @Test(enabled = false)
     public void printTestDataThree()
     {
         System.out.print("This is the dummy test case 3");
+
+    }
+
+    @Test
+    public void  printSingleHeader(){
+
+        singleHeader = getContentTypeHeader ();
+
+        Assert.assertEquals ("application/json", singleHeader);
+
+    }
+
+
+    @Test
+    public void addMoreHeaders(){
+
+        key = "TestHeaderKey";
+        value = "TestHeaderValue";
+
+        //Add new header
+        buildCustomHeaders(key, value);
+
+
+        //Assert.assertEquals (numberMapping, "{TestHeaderKey=TestHeaderValue, Content-Type=application/json}", "The two headersets are equals ");
+
+
+        //print all headers
+        System.out.println("All the headers \t:\t" + numberMapping);
+
+        //print single header
+        System.out.println("One Signle header \t:\t" + numberMapping.get (key));
+
 
     }
 }
